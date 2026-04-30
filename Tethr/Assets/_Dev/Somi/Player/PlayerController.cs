bc45a014dfcace47939f9fcb53511cfce0ed78a5
Somi Euler-Ajayi <66644410+Afronomical@users.noreply.github.com>
2026-04-30 20:07:12 +0100

    General player Controller Creation

---
+++ Tethr/Assets/_Dev/Somi/Player/PlayerController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /*
+     * Player Controller should control what the player can do in any given moment, it needs a reference to:
+     *  The movement component
+     *  The Animation Handler
+     *  The interactor (if one exists)
+     *  The combat system
+     *  
+     * */
+
+
+
+}
---
+++ Tethr/Assets/_Dev/Somi/Player/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.ComponentModel;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    /*
+      As a physics based character this will need to reference the Rigidbody (RB), the sprite for visuals and the player controller
+
+        Standard Movement
+        Achieved by applying a set force on the player left or right non local to the player's orientation.
+        A resistance force should also always be applied unless the player is on a frictionless surface or airborne (otherwise just a reduced force)
+
+        Instead it might be better to give the player values based on their "terrain state"
+        Sliding reduces friction but the player can't increase or decrease speed
+        Running has everything at a standard 
+        Jumping/Falling allows for a small amount of aerial strafing but otherwise has no friction
+
+        To limit the players speed a force is always applied in the opposite direction of their movement 
+        
+        A raycast will be needed to see whether or not the player can jump and what surface they are currently on
+        
+     */
+
+
+    [SerializeField] private float moveSpeed = 5f;
+     private float inputDirection = 0;
+    [SerializeField] private Rigidbody2D rb;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        TryGetComponent<Rigidbody2D>(out rb);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+    }
+}
